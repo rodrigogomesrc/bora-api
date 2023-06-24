703b55ad07fe3000f8e5caa9fce867fea105542e
Rita Lopes <ritacassia.lino@hotmail.com>
2023-06-24 04:27:35 -0300

    add location

---
+++ src/main/java/br/ufrn/bora/service/LocationService.java
@@ -0,0 +1,108 @@
+package br.ufrn.bora.service;
+
+import br.ufrn.bora.domain.Location;
+import br.ufrn.bora.repository.LocationRepository;
+import br.ufrn.bora.service.dto.LocationDTO;
+import br.ufrn.bora.service.mapper.LocationMapper;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+/**
+ * Service Implementation for managing {@link Location}.
+ */
+@Service
+public class LocationService {
+
+    private final Logger log = LoggerFactory.getLogger(LocationService.class);
+
+    private final LocationRepository locationRepository;
+
+    private final LocationMapper locationMapper;
+
+    public LocationService(LocationRepository locationRepository, LocationMapper locationMapper) {
+        this.locationRepository = locationRepository;
+        this.locationMapper = locationMapper;
+    }
+
+    /**
+     * Save a location.
+     *
+     * @param locationDTO the entity to save.
+     * @return the persisted entity.
+     */
+    public LocationDTO save(LocationDTO locationDTO) {
+        log.debug("Request to save Location : {}", locationDTO);
+        Location location = locationMapper.toEntity(locationDTO);
+        location = locationRepository.save(location);
+        return locationMapper.toDto(location);
+    }
+
+    /**
+     * Update a location.
+     *
+     * @param locationDTO the entity to save.
+     * @return the persisted entity.
+     */
+    public LocationDTO update(LocationDTO locationDTO) {
+        log.debug("Request to update Location : {}", locationDTO);
+        Location location = locationMapper.toEntity(locationDTO);
+        location = locationRepository.save(location);
+        return locationMapper.toDto(location);
+    }
+
+    /**
+     * Partially update a location.
+     *
+     * @param locationDTO the entity to update partially.
+     * @return the persisted entity.
+     */
+    public Optional<LocationDTO> partialUpdate(LocationDTO locationDTO) {
+        log.debug("Request to partially update Location : {}", locationDTO);
+
+        return locationRepository
+            .findById(locationDTO.getId())
+            .map(existingLocation -> {
+                locationMapper.partialUpdate(existingLocation, locationDTO);
+
+                return existingLocation;
+            })
+            .map(locationRepository::save)
+            .map(locationMapper::toDto);
+    }
+
+    /**
+     * Get all the locations.
+     *
+     * @return the list of entities.
+     */
+    public List<LocationDTO> findAll() {
+        log.debug("Request to get all Locations");
+        return locationRepository.findAll().stream().map(locationMapper::toDto).collect(Collectors.toCollection(LinkedList::new));
+    }
+
+    /**
+     * Get one location by id.
+     *
+     * @param id the id of the entity.
+     * @return the entity.
+     */
+    public Optional<LocationDTO> findOne(String id) {
+        log.debug("Request to get Location : {}", id);
+        return locationRepository.findById(id).map(locationMapper::toDto);
+    }
+
+    /**
+     * Delete the location by id.
+     *
+     * @param id the id of the entity.
+     */
+    public void delete(String id) {
+        log.debug("Request to delete Location : {}", id);
+        locationRepository.deleteById(id);
+    }
+}

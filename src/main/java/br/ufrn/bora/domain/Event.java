a3303489386b44810900d9c7ee80e42d269996e7
Rita Lopes <ritacassia.lino@hotmail.com>
2023-06-25 17:51:04 -0300

    add event crud

---
+++ src/main/java/br/ufrn/bora/domain/Event.java
@@ -0,0 +1,131 @@
+package br.ufrn.bora.domain;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+/**
+ * A Event.
+ */
+@Document(collection = "event")
+@SuppressWarnings("common-java:DuplicatedBlocks")
+public class Event implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    private String id;
+
+    @Field("title")
+    private String title;
+
+    @Field("organization")
+    private String organization;
+
+    @Field("date")
+    private LocalDate date;
+
+    @DBRef
+    @Field("location")
+    private Location location;
+
+    // jhipster-needle-entity-add-field - JHipster will add fields here
+
+    public String getId() {
+        return this.id;
+    }
+
+    public Event id(String id) {
+        this.setId(id);
+        return this;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public Event title(String title) {
+        this.setTitle(title);
+        return this;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getOrganization() {
+        return this.organization;
+    }
+
+    public Event organization(String organization) {
+        this.setOrganization(organization);
+        return this;
+    }
+
+    public void setOrganization(String organization) {
+        this.organization = organization;
+    }
+
+    public LocalDate getDate() {
+        return this.date;
+    }
+
+    public Event date(LocalDate date) {
+        this.setDate(date);
+        return this;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Location getLocation() {
+        return this.location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public Event location(Location location) {
+        this.setLocation(location);
+        return this;
+    }
+
+    // jhipster-needle-entity-add-getters-setters - JHipster will add getters and setters here
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Event)) {
+            return false;
+        }
+        return id != null && id.equals(((Event) o).id);
+    }
+
+    @Override
+    public int hashCode() {
+        // see https://vladmihalcea.com/how-to-implement-equals-and-hashcode-using-the-jpa-entity-identifier/
+        return getClass().hashCode();
+    }
+
+    // prettier-ignore
+    @Override
+    public String toString() {
+        return "Event{" +
+            "id=" + getId() +
+            ", title='" + getTitle() + "'" +
+            ", organization='" + getOrganization() + "'" +
+            ", date='" + getDate() + "'" +
+            "}";
+    }
+}
---
+++ src/main/java/br/ufrn/bora/service/dto/EventDTO.java
@@ -0,0 +1,95 @@
+package br.ufrn.bora.service.dto;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Objects;
+
+/**
+ * A DTO for the {@link br.ufrn.bora.domain.Event} entity.
+ */
+@SuppressWarnings("common-java:DuplicatedBlocks")
+public class EventDTO implements Serializable {
+
+    private String id;
+
+    private String title;
+
+    private String organization;
+
+    private LocalDate date;
+
+    private LocationDTO location;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getOrganization() {
+        return organization;
+    }
+
+    public void setOrganization(String organization) {
+        this.organization = organization;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocationDTO getLocation() {
+        return location;
+    }
+
+    public void setLocation(LocationDTO location) {
+        this.location = location;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof EventDTO)) {
+            return false;
+        }
+
+        EventDTO eventDTO = (EventDTO) o;
+        if (this.id == null) {
+            return false;
+        }
+        return Objects.equals(this.id, eventDTO.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.id);
+    }
+
+    // prettier-ignore
+    @Override
+    public String toString() {
+        return "EventDTO{" +
+            "id='" + getId() + "'" +
+            ", title='" + getTitle() + "'" +
+            ", organization='" + getOrganization() + "'" +
+            ", date='" + getDate() + "'" +
+            ", location=" + getLocation() +
+            "}";
+    }
+}

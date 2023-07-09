b0bd2510a80ccaf8cdc78ce8d0db1aed0e3c8017
Rodrigo Rocha <rocha.rodrigogomes@gmail.com>
2023-07-09 10:00:34 -0300

    added restrictions to rate event and create some routes on the controller

--- src/main/java/br/ufrn/bora/repository/RatingEventRepository.java
+++ src/main/java/br/ufrn/bora/repository/RatingEventRepository.java
@@ -5,9 +5,18 @@ import br.ufrn.bora.domain.RatingEvent;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * Spring Data MongoDB repository for the Event entity.
  */
 @SuppressWarnings("unused")
 @Repository
-public interface RatingEventRepository extends MongoRepository<RatingEvent, String> {}
+public interface RatingEventRepository extends MongoRepository<RatingEvent, String> {
+
+    List<RatingEvent> findByEventId(String eventId);
+
+    List<RatingEvent> findByUserId(String userId);
+
+    List<RatingEvent> findByEventIdAndUserId(String eventId, String userId);
+}
--- src/main/java/br/ufrn/bora/web/rest/RatingEventResource.java
+++ src/main/java/br/ufrn/bora/web/rest/RatingEventResource.java
@@ -1,9 +1,13 @@
 package br.ufrn.bora.web.rest;
 
 import br.ufrn.bora.domain.RatingEvent;
+import br.ufrn.bora.service.EventRegisterService;
 import br.ufrn.bora.service.RatingEventService;
 import br.ufrn.bora.service.dto.RatingEventDTO;
 import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,27 +15,58 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api")
 public class RatingEventResource {
 
-    //TODO: Add logger
+    private final Logger log = LoggerFactory.getLogger(RatingEventResource.class);
 
     private final RatingEventService ratingEventService;
 
-    public RatingEventResource(RatingEventService ratingEventService) {
+    private final EventRegisterService eventRegisterService;
+
+    public RatingEventResource(RatingEventService ratingEventService, EventRegisterService eventRegisterService) {
         this.ratingEventService = ratingEventService;
+        this.eventRegisterService = eventRegisterService;
     }
 
     @GetMapping("/rating-event")
     public ResponseEntity<List<RatingEvent>> getAll() {
+        log.debug("REST request to get all RatingEvents");
         return ResponseEntity.ok().body(ratingEventService.findAll());
     }
 
     @PostMapping("/rating-event")
     public ResponseEntity<RatingEvent> createRatingEvent(@RequestBody RatingEventDTO ratingEventDTO) {
-        //TODO: verify if said user already rated said event
-        //TODO: verify if the user has gone to the event
+        log.debug("REST request to create RatingEvent : {}", ratingEventDTO);
+
+        List<RatingEvent> ratingEvents = ratingEventService.findByEventIdAndUserId(ratingEventDTO.getEventId(), ratingEventDTO.getUserId());
+        if (!ratingEvents.isEmpty()) {
+            return ResponseEntity.status(409).build();
+        }
+
+        if (!eventRegisterService.hasUserAttendedEvent(ratingEventDTO.getEventId(), ratingEventDTO.getUserId())) {
+            return ResponseEntity.badRequest().build();
+        }
+
         RatingEvent ratingEvent = new RatingEvent();
         ratingEvent.setRating(ratingEventDTO.getRating());
         ratingEvent.setComment(ratingEventDTO.getComment());
         return ResponseEntity.ok().body(ratingEventService.save(ratingEvent));
     }
-    //TODO: add methods to get RatingEvent by event id and user id
+
+    @GetMapping("/rating-event/{eventId}")
+    public ResponseEntity<List<RatingEvent>> getRatingEventByEventId(@PathVariable String eventId) {
+        log.debug("REST request to get RatingEvent by eventId : {}", eventId);
+        return ResponseEntity.ok().body(ratingEventService.findByEventId(eventId));
+    }
+
+    @GetMapping("/rating-event/{eventId}/{userId}")
+    public ResponseEntity<List<RatingEvent>> getRatingEventByEventIdAndUserId(
+        @PathVariable String eventId, @PathVariable String userId) {
+        log.debug("REST request to get RatingEvent by eventId and userId : {}, {}", eventId, userId);
+        return ResponseEntity.ok().body(ratingEventService.findByEventIdAndUserId(eventId, userId));
+    }
+
+    @GetMapping("/rating-event/user/{userId}")
+    public ResponseEntity<List<RatingEvent>> getRatingEventByUserId(@PathVariable String userId) {
+        log.debug("REST request to get RatingEvent by userId : {}", userId);
+        return ResponseEntity.ok().body(ratingEventService.findByUserId(userId));
+    }
 }

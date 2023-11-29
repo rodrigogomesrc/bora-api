8160caad54864972976522c6f120be811ea96aa1
Rodrigo Rocha <rocha.rodrigogomes@gmail.com>
2023-11-29 09:57:10 -0300

    ...

--- src/test/java/br/ufrn/bora/service/mapper/TicketMapperTest.java
+++ src/test/java/br/ufrn/bora/service/mapper/TicketMapperTest.java
@@ -1,16 +1,19 @@
 package br.ufrn.bora.service.mapper;
 
-import static org.assertj.core.api.Assertions.assertThat;
+//import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
 
 class TicketMapperTest {
 
     private TicketMapper ticketMapper;
 
+    /*
     @BeforeEach
     public void setUp() {
         ticketMapper = new TicketMapperImpl();
     }
+
+     */
 }
